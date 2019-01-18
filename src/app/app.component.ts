be5b748f96325cffd73f74b16be7144dd83077a6
Julien DIEMER <julien.diemer@gustaveroussy.fr>
2019-01-18 10:00:12 +0100

    Initialisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormlyFieldConfig} from "@ngx-formly/core";
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  form = new FormGroup({});
+  model = {};
+  fields: FormlyFieldConfig[] = [
+    {
+      //className: 'col-sm-4',
+      type: 'input',
+      key: 'investmentName2',
+      templateOptions: {
+        label: 'Name of Investment:',
+        required: true,
+      },
+    },
+    {
+      key: 'formly-tabs',
+      type: 'card',
+      fieldGroup: [
+        {
+          templateOptions: {
+            label: 'First tab'
+          },
+          fieldGroup: [
+            {
+              //className: 'col-sm-4',
+              type: 'input',
+              key: 'investmentName',
+              templateOptions: {
+                label: 'Name of Investment:',
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          templateOptions: {
+            label: 'Second tab'
+          },
+          fieldGroup: [
+            {
+              type: 'input',
+              key: 'investmentDate',
+              //className: 'col-sm-3',
+              templateOptions: {
+                type: 'date',
+                label: 'Date of Investment:',
+              },
+            },
+          ],
+        }
+      ],
+    },
+  ];
+
+  submit(model) {
+    console.log(model);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormlyModule} from "@ngx-formly/core";
+import {ReactiveFormsModule} from "@angular/forms";
+import {FormlyBootstrapModule} from '@ngx-formly/bootstrap';
+import {CardComponent} from "./type/card/card.component";
+import {CardTabComponent} from "./type/card-tab/card-tab.component";
+import {CardTabsComponent} from "./type/card-tabs/card-tabs.component";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardTabComponent,
+    CardTabsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot({
+      types: [
+        {name: 'card', component: CardComponent},
+      ],
+    }),
+    FormlyBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
